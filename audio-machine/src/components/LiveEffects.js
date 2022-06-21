68ba6de1a1762371b24157e74dbdfc5d558bf17f
Brendan Ward <kingbeebop@gmail.com>
2022-06-21 11:08:55 -0400

    even more

--- audio-machine/src/components/LiveEffects.js
+++ audio-machine/src/components/LiveEffects.js
@@ -1,2 +1,15 @@
+import Effect from "./Effect"
 
+function LiveEffects( { effects }){
+    return(
+        <div>
+            {effects.map(effect=>{
+                return(
+                    <Effect effect={effect} />
+                )
+            })}
+        </div>
+    )
+}
 
+export default LiveEffects
